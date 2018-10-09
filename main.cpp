cc2e8d0dfe476a67635df8808e57291f531cf9c6
marcogarciamuro <magarciamuro@mail.csuchico.edu>
2018-10-08 19:05:05 -0700

    Changed string into vector

--- main.cpp
+++ main.cpp
@@ -1,42 +1,45 @@
 //Author: Marco Garcia
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 
 int main()
 {
-        string list[5]; //array of 5 strings
-        int numItems = 0;
-        char input;
-        string NewItem;
-        
-        
-        do
-        {
-                cout<<"\n==GROCERY LIST MANAGER==";
-                cout<<"\nEnter your choice: ";
-                cout<<"\n (A)dd an item";
-                cout<<"\n (Q)uit";
-                cout<<"\nYour choice (A/Q): ";
-                cin>>input;
-                if(input =='a'||input =='A')
-                {      
-                        cout<<"What is the item?\n";
-                               cin>>NewItem;
-                               if(numItems<5)
-                               {list[numItems] = NewItem;
-                               numItems++;
-                               }
-                               else
-                               {cout<<"You'll need a bigger list!\n";}
+  vector<string> list;
+  int numItems = 0;
+  char input;
+  string NewItem; 
 
-                 }
-        }
-        while(input!='q' && input!='Q');
-        cout<<"==ITEMS TO BUY==\n";
-                               for(int index=0;index<5;index++)
-                                   cout<<index+1<<" "<<list[index]<<endl;
-                                   
+  do
+  {
+    cout<<"\n==GROCERY LIST MANAGER==";
+    cout<<"\nEnter your choice: ";
+    cout<<"\n (A)dd an item";
+    cout<<"\n (Q)uit";
+    cout<<"\nYour choice (A/Q): ";
+    cin>>input;
+    if(input =='a'||input =='A')
+    {      
+      cout<<"What is the item?\n";
+      cin>>NewItem;
+      list.push_back(NewItem);
+    }
+  } while(input!='q' && input!='Q');
 
-        return 0;
+  if(list.size()==0)
+  {
+    cout<<"No items to buy!\n";
+  } 
+  else
+  {
+    cout<<"==ITEMS TO BUY==\n";
+    for(int index=0;index<list.size();index++)
+    {
+      cout<<index+1<<" "<<list[index]<<endl;
+    }
+  }
+
+
+  return 0;
 }
